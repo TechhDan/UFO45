f9e479bfa7203b2da64e6c0130a1220302a16d00
TechhDan <tech.dan2689@gmail.com>
2020-06-21 13:35:51 -0400

    Update

--- connect.php
+++ connect.php
@@ -1,8 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "advigilo2689";
-$dbname = "ufo45";
+
+require_once __DIR__ . '/credentials.php';
+
+$servername = DB_HOST;
+$username = DB_USER;
+$password = DB_PASSWORD;
+$dbname = DB_NAME;
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
